90b2a0eb43156f9b56baad790b624a92c36c8355
Sup3rSwee10 <160248336+Sup3rSwee10@users.noreply.github.com>
2024-03-15 20:42:52 +0400

    Update 1 лаба.py

--- 1 лаба.py
+++ 1 лаба.py
@@ -1,35 +1,47 @@
 number_dict = {0: 'ноль',
-          1: 'один',
-          2: 'два',
-          3: 'три',
-          4: 'четыре',
-          5: 'пять',
-          6: 'шесть',
-          7: 'семь',
-          8: 'восемь',
-          9: 'девять'}
+		1: 'один',
+		2: 'два',
+		3: 'три',
+		4: 'четыре',
+		5: 'пять',
+		6: 'шесть',
+		7: 'семь',
+		8: 'восемь',
+		9: 'девять'}
+
 f = open('text.txt')
+
 s = f.read().split()
+f1 = False
 a = []
 q = ""
 k = int(input("Введите число k: "))
 
-x = "" 
+x = ""
 
 for i in s:
-    for j in i:
-        if j not in x:
-            x += j
-        else:
-            continue
-    if int(i[-1]) % 2 != 0 and len(x) <= k:
-        q += x
-        a.append(i)
-    x = ""
-
-print("Нечётные числа, использующие не более К разных цифр:")
-print(*a, "\n") 
-print("Список используемых цифр: ")
-q = sorted(list(set(q)))
-for i in q:
-    print(number_dict[int(i)] , end=" ")
+	for j in i:
+		if j not in x:
+			x += j
+		else:
+			continue
+	try:
+		if int(i[-1]) % 2 != 0 and len(x) <= k:
+			q += x
+			a.append(i)
+			if i.isdigit():
+				f1 = True
+			else:
+				f1 = False
+		x = ""
+	except ValueError:
+		pass
+if f1:
+	print("Нечётные числа, использующие не более К разных цифр:")
+	print(*a, "\n")
+	print("Список используемых цифр: ")
+	q = sorted(list(set(q)))
+	for i in q:
+		print(number_dict[int(i)] , end=" ")
+else:
+	print("\nФайл text.txt пустой или в нём нет чисел, удовлетворяющих условию. Попробуйте ещё раз\n")
